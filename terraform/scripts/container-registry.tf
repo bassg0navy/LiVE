4b638c60e2d20b203bb9fcd51ac3c212d6d58077
kabass <kenneth.bass@oracle.com>
2022-04-23 07:27:27 -0400

    container repo name correction

--- terraform/scripts/container-registry.tf
+++ terraform/scripts/container-registry.tf
@@ -1,14 +1,14 @@
 // Adds preexisting container registry to LiVE stack
-resource "oci_artifacts_container_repository" "LiVE_container_repo" {
+resource "oci_artifacts_container_repository" "container_repo" {
     compartment_id = var.root_compartment_id
     display_name = var.container_repository_display_name
 }
 
 // Add outputs
 output "registry_state" {
-    value = oci_artifacts_container_repository.LiVE_container_repo.state
+    value = oci_artifacts_container_repository.container_repo.state
 }
 
 output "registry_id" {
-    value = oci_artifacts_container_repository.LiVE_container_repo.id
+    value = oci_artifacts_container_repository.container_repo.id
 }
