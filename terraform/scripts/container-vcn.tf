1386937fb3b0c683fa34ca8ed0859187765d48bb
kabass <kenneth.bass@oracle.com>
2022-04-24 17:28:35 -0400

    add discorvered resources; consolidate .tf's

--- terraform/scripts/container-vcn.tf
+++
@@ -1,9 +0,0 @@
-resource "oci_core_vcn" "precog_vcn" {
-    // Required
-    compartment_id = var.root_compartment_id
-
-    // Optional
-    cidr_blocks = var.vcn_cidr_blocks
-    display_name = var.vcn_display_name
-    dns_label = var.vcn_dns_label
-}
