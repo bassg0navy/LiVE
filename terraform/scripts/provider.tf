feabf998a78d25a61833cb49fa88537aa381a322
kabass <kenneth.bass@oracle.com>
2022-04-24 17:31:32 -0400

    add discovered tf files

---
+++ terraform/scripts/provider.tf
@@ -0,0 +1,8 @@
+provider "oci" {
+  version = "4.71.0"
+  region = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+}
