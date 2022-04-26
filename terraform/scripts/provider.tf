98084825abfaa574bc307b9fd9915db90f4e41a6
kabass <kenneth.bass@oracle.com>
2022-04-26 02:01:39 -0400

    post-rebase | fix variables,provider and rm vars.tf

--- terraform/scripts/provider.tf
+++ terraform/scripts/provider.tf
@@ -1,3 +1,13 @@
-provider oci {
-	region = var.region
+provider "oci" {
+  version = "4.71.0"
+  region = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  config_file_profile= "LiVE"
+}
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  //config_context = "my-context"
 }
