204f170ae52a64bfe98aeb80cbe994bd6005d578
kabass <kenneth.bass@oracle.com>
2022-04-26 01:46:20 -0400

    add vars.tf to gitignor (duplicate vars preventing tf init)

--- terraform/scripts/vars.tf
+++
@@ -1,3 +0,0 @@
-variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaadkxkk76ljinkchc3gmtgohryrbegiiaakcyrnhgyuy6a7iutlmtq" }
-variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaadkxkk76ljinkchc3gmtgohryrbegiiaakcyrnhgyuy6a7iutlmtq" }
-variable region { default = "us-ashburn-1" }
