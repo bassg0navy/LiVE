ac1979acb3cf7a198ff6bd3674336b3e93bc2cb6
kabass <kenneth.bass@oracle.com>
2022-04-27 00:16:47 -0400

    rm config file profile for provider to counter stack plan err

--- terraform/scripts/provider.tf
+++ terraform/scripts/provider.tf
@@ -1,9 +1,9 @@
 provider "oci" {
-  //version = "4.71.0"
+  //version = "4.72.0"
   region = var.region
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
   private_key_path = var.private_key_path
-  config_file_profile= "LiVE"
+  //config_file_profile= "LiVE"
 }
